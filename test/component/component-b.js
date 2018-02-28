5269ba901c8a7486513eebe147cf13c41f801bb7
_asdx_ <xasdx@tuta.io>
2018-02-28 10:53:14 +0000

    Reject components with duplicate names

---
+++ test/component/component-b.js
@@ -0,0 +1,9 @@
+import { Component } from "~"
+
+@Component
+class ComponentA {
+
+  hi() {
+    return "hi"
+  }
+}
--- test/index.test.js
+++ test/index.test.js
@@ -1,17 +1,21 @@
 import { expect } from "chai"
 
+let { containr } = require("~")
+
 module.exports = {
+  "beforeEach": () => containr.instances = [],
   "Component decorator": {
-    "registers instances in the container": done => {
-      
-      let { assembled } = require("~")
+    "registers instances in the container": () => {
       require("./component/component-a")
-      
-      assembled(containr => {
-        let res = containr.component("componenta").hello()
-        expect(res).to.equal("hello")
-        done()
-      })
+      let res = containr.component("componenta").hello()
+      expect(res).to.equal("hello")
+
+    },
+    "rejects components with duplicate names": () => {
+      delete require.cache[require.resolve("./component/component-a")]
+      delete require.cache[require.resolve("./component/component-b")]
+      require("./component/component-a")
+      expect(() => require("./component/component-b")).to.throw()
     }
   }
 }
